4fbd1ef2a11e0ca52e68b487e5041bcf449599fe
Shelmith Muthoni <muthonishelmith136@gmail.com>
2025-01-17 17:36:36 +0300

    Completing Looping Lap.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+const { getModulesPluginNames } = require("@babel/preset-env");
+
 // Code your solutions in this file
+function writeCards(names, event) {
+    const messages = [];//empty array to store thank you messages
+
+    for (let i = 0;i < names.length; i++) { //loop thru the names array
+        //thankyou messages for each of the message array
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages;//Return the message (THANK YOU, ${names[i]}FOR THE WONDERFUL GIFT)
+}
+//calling the function to output the results on console
+console.log(writeCards(["Guadalupe","Ollie","Aki"],"birthday"));
+
+function countDown(startNumber) {
+    while (startNumber >= 0) { //Loop until the startNumber reaches 0(condition of the loop)
+        debugger; 
+        console.log(startNumber);
+        startNumber--;//Decrement the number (updation of the loop) 
+    }
+}
+//call the countDown function with a starting number
+countDown(10);
